b3eb86f7f5ad2f7d3a5397acedc459464b21638c
Michael <michael.crothersmc@icloud.com>
2020-07-30 23:12:57 -0500

    Hi

---
+++ Assets/Scripts/ConstantVelocity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantVelocity : MonoBehaviour
+{
+    [SerializeField]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //FixedUpdate is Called 100 times each second
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += new Vector3(0.5f,0,0);
+    }
+}
